6405355ca21f83b0387de0937cba8d34b855642c
ShinkaM <mori.shinka@gmail.com>
2022-04-06 15:27:42 -0400

    made script

---
+++ collect_data.sh
@@ -0,0 +1,132 @@
+#TRAUMA & ABUSE
+python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/trauma/ -l 10000  -t gender -s Anger
+python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/trauma/ -l 10000  -t gender -s survivorsofabuse
+python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/trauma/ -l 10000  -t gender -s domesticviolence
+python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/trauma/ -l 10000  -t gender -s ptsd
+python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/trauma/ -l 10000  -t gender -s rapecounceling
+python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/trauma/ -l 10000  -t gender -s selfharm
+python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/trauma/ -l 10000  -t gender -s StopSelfHarm
+python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/trauma/ -l 10000  -t gender -s traumatoolbox
+python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/trauma/ -l 10000  -t gender -s rape
+python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/trauma/ -l 10000  -t gender -s MenGetRapedToo
+
+
+
+# #PSYCHOSIS & ANXIETY
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/psychosis/ -l 10000  -t gender -s Anxiety
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/psychosis/ -l 10000  -t gender -s socialanxiety
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/psychosis/ -l 10000  -t gender -s Anxietyhelp
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/psychosis/ -l 10000  -t gender -s HealthAnxiety
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/psychosis/ -l 10000  -t gender -s BPD
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/psychosis/ -l 10000  -t gender -s dpdr
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/psychosis/ -l 10000  -t gender -s schizophernia
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/psychosis/ -l 10000  -t gender -s schizoaffective
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/psychosis/ -l 10000  -t gender -s emetophobia
+
+# # COMPULSIVE DISORDERS
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/compulsive/ -l 10000  -t gender -s CompulsiveSkinPicking
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/compulsive/ -l 10000  -t gender -s OCD
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/compulsive/ -l 10000  -t gender -s Trichsters
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/compulsive/ -l 10000  -t gender -s DysmorphicDisorder
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/compulsive/ -l 10000  -t gender -s OCPD
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/compulsive/ -l 10000  -t gender -s OCD
+
+
+
+# # #COPING & THERAPY
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s getting_over_it
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s helpmecope
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s offmychest
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s mmfb
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s askatherapist
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s EOOD
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s dbtselfhelp
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s AlAnon
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s REDDITORSINRECOVERY
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s GetMotivated
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s Antipsychiatry
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s selfhelp
+
+# #MOOD DISORDERs 
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s depression
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s depression_help
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s ForeverAlone
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s GFD
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s mentalhealth
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s suicidewatch
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s sad
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s BipolarReddit
+
+# #ADDICTION 
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s stopdrinking
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s addiction
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s stopsmoking
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s leaves
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s alcoholism
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s cripplingalcoholism
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s quittingkratom
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s alcoholicsanonymous
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s NoFap
+
+# #EATING & BODY
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s eating_disorders
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s EatingDisorders
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s ARFID
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s fuckeatingdisorders
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s BodyAcceptance
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s bulimia
+# ##extra
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s edrecovery
+
+
+
+# #NEURODEVELOPMENTAL DISORDERS
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s ADHD
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s aspergers
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s TwoXADHD
+
+# #GENERAL
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s AskDocs
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s needadvice
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s Advice
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s mentalillness
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s neurodiversity
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s whatsbotheringyou
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s MadOver30
+
+# #BROAD SOCIAL
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s AtheistTwelveSteppers
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s relationship_advice
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s nosurf
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s Needafriend
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s PCOS
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s disability
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s Tourettes
+
+# ##extra
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s relationships
+
+# #GENDER
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s TwoXChromosomes
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s AskMen
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s AskWomen
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s askgaybros
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s askredditafterdark
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s asktransgender
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s askmenover30
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s transgender
+# python3 rel_advice_collect.py -d ../../../../local/reddit_demographic/ -l 10000  -t gender -s lgbt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
